9025629922040e29d11455328a6f576979e92446
sam33 <sup3355@gmail.com>
2017-03-07 12:13:38 +0800

    second commit

--- RemovingBackgrounds/Form1.Designer.cs
+++ RemovingBackgrounds/Form1.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.w_box = new System.Windows.Forms.TextBox();
+            this.h_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "載入圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::RemovingBackgrounds.Properties.Resources.image_20170131_171643_67824;
+            this.pictureBox1.Location = new System.Drawing.Point(172, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 315);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 304);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(154, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "輸出檔案";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // w_box
+            // 
+            this.w_box.Enabled = false;
+            this.w_box.Location = new System.Drawing.Point(12, 276);
+            this.w_box.Name = "w_box";
+            this.w_box.Size = new System.Drawing.Size(64, 22);
+            this.w_box.TabIndex = 15;
+            // 
+            // h_box
+            // 
+            this.h_box.Enabled = false;
+            this.h_box.Location = new System.Drawing.Point(102, 276);
+            this.h_box.Name = "h_box";
+            this.h_box.Size = new System.Drawing.Size(64, 22);
+            this.h_box.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 12);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Threshold:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 12);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Click to remove background -->";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(66, 49);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(100, 45);
+            this.trackBar1.TabIndex = 21;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(66, 225);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(100, 45);
+            this.trackBar2.TabIndex = 23;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 12);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Scale:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(769, 517);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.h_box);
+            this.Controls.Add(this.w_box);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Remove Background";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox w_box;
+        private System.Windows.Forms.TextBox h_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- RemovingBackgrounds/Form1.cs
+++ RemovingBackgrounds/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace RemovingBackgrounds
 {
@@ -15,6 +17,253 @@ namespace RemovingBackgrounds
         public Form1()
         {
             InitializeComponent();
+            detector = new BackgroundDetector();
+            trackBar1.Maximum = 255;
+            trackBar1.Minimum = 0;
+            trackBar2.Minimum = 50;
+            trackBar2.Maximum = 200;
+            trackBar2.Value = 100;
+        }
+        
+        public static Bitmap resizeImage(Bitmap imgToResize, Size size)
+        {
+            return new Bitmap(imgToResize, size);
+        }
+
+        public Bitmap StreampTo32bppArgbBitmap(Stream myStream)
+        {
+            Bitmap orig;
+            Bitmap newimg;
+            using (myStream)
+            {
+                orig = new Bitmap(myStream);
+                newimg = new Bitmap(orig.Width, orig.Height, PixelFormat.Format32bppArgb);
+                using (Graphics gr = Graphics.FromImage(newimg))
+                {
+                    gr.DrawImage(orig, new Rectangle(0, 0, orig.Width, orig.Height));
+                }
+            }
+            return newimg;
+        }
+
+        Bitmap image;
+        int w;
+        int h;
+        int t;
+        BackgroundDetector detector;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "JPG (*.jpg)|*.jpg|PNG (*.png)|*.png";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            String FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileName = openFileDialog1.FileName;
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        image = StreampTo32bppArgbBitmap(myStream);
+                        myStream.Close();
+                        w_box.Text = (w=image.Width) + "";
+                        h_box.Text = (h=image.Height) + "";
+                        pictureBox1.Width = image.Width;
+                        pictureBox1.Height = image.Height;
+                        //image = resizeImage(image, new Size(pictureBox1.Width, pictureBox1.Height));
+                        pictureBox1.Image = image;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法開啟" + FileName + ":\n" + ex.Message);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Bitmap temp = new Bitmap(image);
+            detector.detect(e.X, e.Y, trackBar1.Value, image, temp);
+            image = temp;
+            pictureBox1.Image = image;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "PNG files (*.png)|*.png";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+            String FileName = "";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    try
+                    {
+                        if (image != null)
+                        {
+                            FileName = saveFileDialog1.FileName;
+                            int ww = (int)((float)image.Width * ((float)trackBar2.Value / 100.0f));
+                            int hh = (int)((float)image.Height * ((float)trackBar2.Value / 100.0f));
+                            image = resizeImage(image, new Size(ww, hh));
+                            image.Save(myStream, ImageFormat.Png);
+                        }
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show("無法寫入" + FileName + ":\n" + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            int ww = (int)((float)image.Width * ((float)trackBar2.Value / 100.0f));
+            int hh = (int)((float)image.Height * ((float)trackBar2.Value / 100.0f));
+            image = resizeImage(image, new Size(ww, hh));
+            pictureBox1.Width = ww;
+            pictureBox1.Height = hh;
+            pictureBox1.Image = image;
+            w_box.Text = ww + "";
+            h_box.Text = hh + "";
+        }
+    }
+
+    class BackgroundDetector
+    {
+        private class entry
+        {
+            public int x;
+            public int y;
+            public entry(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        Color target_color;
+        Bitmap bmp;
+        Boolean[,] is_background;
+        List<entry> BFS_array;
+        Boolean[,] go;
+        private int BFS(int t)
+        {
+            entry e = BFS_array.ElementAt(0);
+            BFS_array.RemoveAt(0);
+
+            int d = 0;
+            int A = bmp.GetPixel(e.x, e.y).A;
+            int R = bmp.GetPixel(e.x, e.y).R;
+            int G = bmp.GetPixel(e.x, e.y).G;
+            int B = bmp.GetPixel(e.x, e.y).B;
+            int tA = target_color.A;
+            int tR = target_color.R;
+            int tG = target_color.G;
+            int tB = target_color.B;
+            int dA = Math.Abs(A - tA);
+            int dR = Math.Abs(R - tR);
+            int dG = Math.Abs(G - tG);
+            int dB = Math.Abs(B - tB);
+            d = dA + dR + dG + dB;
+
+
+            if (Math.Abs(d)<t)
+            {
+                is_background[e.x,e.y] = true;
+                if ((e.x + 1) < bmp.Width)
+                {
+                    if (!is_background[e.x + 1,e.y])
+                    {
+                        if (!go[e.x + 1,e.y])
+                        {
+                            //加到List裡面代表遲早會執行,直接視為一經處理過,防止List前面的元素在重複加入
+                            go[e.x + 1,e.y] = true;
+                            BFS_array.Add(new entry(e.x + 1, e.y));
+                        }
+                    }
+                }
+                if ((e.y + 1) < bmp.Height)
+                {
+                    if (!is_background[e.x,e.y + 1])
+                    {
+                        if (!go[e.x,e.y + 1])
+                        {
+                            go[e.x,e.y + 1] = true;
+                            BFS_array.Add(new entry(e.x, e.y + 1));
+                        }
+                    }
+                }
+                if ((e.x - 1) >= 0)
+                {
+                    if (!is_background[e.x - 1,e.y])
+                    {
+                        if (!go[e.x - 1,e.y])
+                        {
+                            go[e.x - 1,e.y] = true;
+                            BFS_array.Add(new entry(e.x - 1, e.y));
+                        }
+                    }
+                }
+                if ((e.y - 1) >= 0)
+                {
+                    if (!is_background[e.x,e.y - 1])
+                    {
+                        if (!go[e.x,e.y - 1])
+                        {
+                            go[e.x,e.y - 1] = true;
+                            BFS_array.Add(new entry(e.x, e.y - 1));
+                        }
+                    }
+                }
+            }
+            return BFS_array.Count;
+        }
+
+        public void detect(int x,int y,int t,Bitmap RGBA_bitmap, Bitmap RGBA_result)
+        {
+            target_color = RGBA_bitmap.GetPixel(x, y);
+            this.bmp = RGBA_bitmap;
+            is_background = new Boolean[bmp.Width,bmp.Height];
+            this.go = new Boolean[bmp.Width, bmp.Height];
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    is_background[i,j] = false;
+                    go[i,j] = false;
+                }
+            }
+            BFS_array = new List<entry>();
+            go[x,y] = true;
+            BFS_array.Add(new entry(x, y));
+            int c;
+            while ((c = BFS(t)) > 0)
+            {
+                //Log.d("ccc","size="+c);
+            }
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    if (is_background[i,j])
+                    {
+                        RGBA_result.SetPixel(i, j, Color.Transparent);
+                    }
+                    else
+                    {
+                        RGBA_result.SetPixel(i, j, bmp.GetPixel(i, j));
+                    }
+                }
+            }
         }
     }
 }
